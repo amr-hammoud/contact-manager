0eb39d0ab1a80e57a5cf84bc225ffe9afe4ea0cf
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-08-05 23:59:15 +0300

    create empty components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import ContactMap from "./Pages/ContactMap";
+import E404 from "./Pages/E404";
+import Header from "./Components/Header";
+import Sidebar from "./Components/Sidebar";
 
 function App() {
-	return <div>Hello World!</div>;
+	return (
+		<div>
+			<Header />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/map" element={<ContactMap />} />
+					<Route path="*" element={<E404 />} />
+				</Routes>
+			</BrowserRouter>
+			<Sidebar />
+		</div>
+	);
 }
 
 export default App;
