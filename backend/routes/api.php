fe928263f8fbeae500cc83f803e6c2c23e48a32d
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-08-06 22:04:35 +0300

    fix bug in api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,5 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'contact'], function() {
     Route::get("{id?}", [ContactController::class, "get"]);
     Route::post("createOrUpdate/{id?}", [ContactController::class, "createOrUpdate"]);
-    Route::delete("delete/{id}", [ContactController::class, "createOrUpdate"]);
+    Route::delete("delete/{id}", [ContactController::class, "delete"]);
 });
