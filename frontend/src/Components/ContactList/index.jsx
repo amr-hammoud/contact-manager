faf6efb5c6ece61c852b4bd9019f4c769bb747e5
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-08-06 12:06:38 +0300

    add styling to contactlist

--- frontend/src/Components/ContactList/index.jsx
+++ frontend/src/Components/ContactList/index.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import './style.css'
+import edit_icon from '../../assets/icons/edit.svg'
+import delete_icon from '../../assets/icons/delete.svg'
 
 const ContactList = ({contacts}) => {
   return (
-    <div>
-      <ul>
-        {contacts.map((contact, index)=>(<li key={index} >{contact.name}</li>))}
-      </ul>
+    <div className='container'>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Phone Number</th>
+            <th>Location</th>
+            <th>a</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact, index)=>(
+          <tr key={index} className='contact-row' >
+            <td>{contact.name}</td>
+            <td>{contact.phone_number}</td>
+            <td>{contact.latitude} - {contact.longitude}</td>
+            <td className='actions'>
+              <img src={edit_icon} alt="edit" />
+              <img src={delete_icon} alt="delete" />
+            </td>
+          </tr>
+          ))}
+        </tbody>
+      </table>
+
     </div>
   )
 }
