be24e8286fc181124736756cde402fa5d8a8e30f
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-08-06 23:14:33 +0300

    implement contactsmap

--- frontend/src/Pages/ContactMap/index.jsx
+++ frontend/src/Pages/ContactMap/index.jsx
@@ -1,16 +1,51 @@
-import React from 'react'
-import Sidebar from '../../Components/Sidebar'
-import './style.css'
+import React, { useEffect, useState } from "react";
+import Sidebar from "../../Components/Sidebar";
+import "./style.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import axios from "axios";
 
 function ContactMap() {
-  return (
-    <div className="all">
-      <Sidebar />
-      <div className="map">
-        <h1>Contacts Map</h1>
-      </div>
-    </div>
-  )
+	const [contacts, setContacts] = useState([]);
+
+	const fetchContacts = async () => {
+		const response = await axios("http://127.0.0.1:8000/api/contact");
+		setContacts(response.data);
+	};
+
+	useEffect(() => {
+		fetchContacts();
+	}, []);
+
+	return (
+		<div className="all">
+			<Sidebar />
+			<div className="map">
+				<h1>Contacts Map</h1>
+				<div id="map">
+					<MapContainer
+						center={[33.89332, 35.50157]}
+						zoom={10}
+						scrollWheelZoom={true}
+					>
+						<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+						{contacts.map((contact, index) => (
+              <div key={index}>
+              <Marker position={[contact.latitude, contact.longitude]}>
+							<Popup>
+								<h4>{contact.name}</h4>
+								<h5>{contact.phone_number}</h5>
+							</Popup>
+						</Marker>
+            </div>
+
+						))}
+				
+					</MapContainer>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default ContactMap
+export default ContactMap;
