2e55e88e23fd50f80ef572f62f29a779b2f04890
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-08-07 00:54:04 +0300

    add custom icon to map

--- frontend/src/Pages/ContactMap/index.jsx
+++ frontend/src/Pages/ContactMap/index.jsx
@@ -3,6 +3,8 @@ import Sidebar from "../../Components/Sidebar";
 import "./style.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import axios from "axios";
+import L from 'leaflet';
+import person_icon from '../../assets/icons/person_pin_circle.svg'
 
 function ContactMap() {
 	const [contacts, setContacts] = useState([]);
@@ -16,6 +18,12 @@ function ContactMap() {
 		fetchContacts();
 	}, []);
 
+	const icon = new L.Icon({
+		iconUrl: person_icon,
+		iconSize: [36,36],
+		iconAnchor: [21,21],
+	})
+
 	return (
 		<div className="all">
 			<Sidebar />
@@ -24,23 +32,27 @@ function ContactMap() {
 				<div id="map">
 					<MapContainer
 						center={[33.89332, 35.50157]}
-						zoom={10}
+						zoom={2}
 						scrollWheelZoom={true}
 					>
 						<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
 						{contacts.map((contact, index) => (
-              <div key={index}>
-              <Marker position={[contact.latitude, contact.longitude]}>
-							<Popup>
-								<h4>{contact.name}</h4>
-								<h5>{contact.phone_number}</h5>
-							</Popup>
-						</Marker>
-            </div>
-
+							<div key={index}>
+								<Marker
+									position={[
+										contact.latitude,
+										contact.longitude,
+									]}
+									icon={icon}
+								>
+									<Popup>
+										<h4>{contact.name}</h4>
+										<h5>{contact.phone_number}</h5>
+									</Popup>
+								</Marker>
+							</div>
 						))}
-				
 					</MapContainer>
 				</div>
 			</div>
